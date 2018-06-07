474a6f892a0b2b231218a4c6a0af8993af47fe9a
dotzipfile <davenickmalone@gmail.com>
2018-06-07 13:05:37 +0100

    Created base route, added connection to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-const hapi = require('hapi');
+/*
+ *  Entry point for API
+ */
 
+// Native Dependencies
+const fs = require('fs');
+
+// External Dependencies
+const hapi = require('hapi');
+const mongoose = require('mongoose');
+
+// Read in config details such as username, password and url for mongodb
+const sensitiveDataFile = fs.readFileSync('sensitive-data.json', 'utf8');
+const configDetails = JSON.parse(sensitiveDataFile);
+const mongoDetails = configDetails.mongoDetails;
+
+// Connect to db using config details
+mongoose.connect(`mongodb://${mongoDetails.username}:${mongoDetails.password}@${mongoDetails.url}`);
+
+// Log successful connection
+mongoose.connection.once('open', () => {
+  console.log('Connected to db');
+});
+
+// Create server
 const server = hapi.server({
   port: 4000,
   host: 'localhost'
 });
 
+// Initialise server
 const init = async() => {
+  // Define / 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      return `<h1>My GraphQL API`;
+    }
+  });
+
+  // Start server
   await server.start();
   console.log(`Server running at: ${ server.info.uri }`);
 };
 
+// Call init
 init();
