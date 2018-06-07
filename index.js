c967f83a139632ff3b3790fbf038ed2cee5d8d32
dotzipfile <davenickmalone@gmail.com>
2018-06-07 17:34:07 +0100

    Added some swagger documentation

--- index.js
+++ index.js
@@ -9,15 +9,25 @@ const fs = require('fs');
 const hapi = require('hapi');
 const mongoose = require('mongoose');
 const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const Inert = require('inert');
+const HapiSwagger = require('hapi-swagger');
+const Vision = require('vision');
 
 // Local dependencies
 const Painting = require('./models/Painting');
 const schema = require('./graphql/schema');
+const Pack = require('./package');
 
 // Read in config details such as username, password and url for mongodb
 const sensitiveDataFile = fs.readFileSync('sensitive-data.json', 'utf8');
 const configDetails = JSON.parse(sensitiveDataFile);
 const mongoDetails = configDetails.mongoDetails;
+
+// Create server
+const server = hapi.server({
+  port: 4000,
+  host: 'localhost'
+});
 
 // Connect to db using config details
 mongoose.connect(`mongodb://${mongoDetails.username}:${mongoDetails.password}@${mongoDetails.url}`);
@@ -27,49 +37,22 @@ mongoose.connection.once('open', () => {
   console.log('Connected to db');
 });
 
-// Create server
-const server = hapi.server({
-  port: 4000,
-  host: 'localhost'
-});
-
 // Initialise server
 const init = async() => {
-  // Define routes
-  server.route([
+  // Register Inert plugin
+  await server.register([
+    Inert,
+    Vision,
     {
-      method: 'GET',
-      path: '/',
-      handler: (request, reply) => {
-        return `<h1>My GraphQL API</h1>`;
-      }
-    },
-    {
-      method: 'GET',
-      path: '/api/v1/paintings',
-      handler: (req, reply) => {
-        return Painting.find();
-      }
-    },
-    {
-      method: 'POST',
-      path: '/api/v1/paintings',
-      handler: (req, reply) => {
-        const { name, url, techniques } = req.payload;
-        const painting = new Painting({
-          name,
-          url,
-          techniques
-        });
-
-        return painting.save();
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Paintings API Documentation',
+          version: Pack.version
+        }
       }
     }
   ]);
-
-  // Start server
-  await server.start();
-  console.log(`Server running at: ${ server.info.uri }`);
 
   // Register hapi-graphql plugin
   await server.register({
@@ -98,7 +81,51 @@ const init = async() => {
       }
     }
   });
+
+  // Define routes
+  server.route([
+    {
+      method: 'GET',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get all the paintings.',
+        tags: [ 'api', 'v1', 'painting' ]
+      },
+      handler: (req, reply) => {
+        return Painting.find();
+      }
+    },
+    {
+      method: 'POST',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get a specific painting by ID.',
+        tags: [ 'api', 'v1', 'painting' ]
+      },
+      handler: (req, reply) => {
+        const { name, url, technique } = req.payload;
+        const painting = new Painting({
+          name,
+          url,
+          technique
+        });
+
+        return painting.save();
+      }
+    }
+  ]);
+
+  // Start server
+  await server.start();
+  console.log(`Server running at: ${ server.info.uri }`);
 };
+
+process.on('unHandledRejection', (err) => {
+  if(err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
 
 // Call init
 init();
